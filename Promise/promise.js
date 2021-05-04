aea00a3327c11c322af17d5bd764fdcc4a93fe1e
taolzz <taolz@outlook.com>
2021-05-04 21:18:15 +0800

    reject 和 resolve 结构搭建和实现

--- Promise/promise.js
+++ Promise/promise.js
@@ -1,17 +1,37 @@
 // 构造函数
 function Promise(executor) {
+  //添加属性
+  this.PromiseState = 'pending'
+  this.PromiseResult = null
+  // 设置this
+  const self = this
+
   // resolve 函数
   function resolve(data) {
-
+    // 只能修改一次 状态
+    if (self.PromiseState !== 'pending') return;
+    // 修改状态并设置结果值
+    self.PromiseState = 'fulfilled'
+    self.PromiseResult = data
   }
 
   // reject 函数
   function reject(data) {
-
+    // 只能修改一次 状态
+    if (self.PromiseState !== 'pending') return;
+    // 修改状态并设置结果值
+    self.PromiseState = 'fulfilled'
+    self.PromiseResult = data
   }
 
-  // 同步调用执行器函数
-  executor(resolve, reject)
+  // 捕获异常 处理
+  try {
+    // 同步调用执行器函数
+    executor(resolve, reject)
+  } catch (e) {
+    reject(e)
+  }
+
 }
 
 // then 方法
