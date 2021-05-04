bab3e8a594c473feafbb35c3386090f432e3ee1c
taolzz <taolz@outlook.com>
2021-05-05 00:07:29 +0800

    then 方法完成

--- Promise/promise.js
+++ Promise/promise.js
@@ -3,6 +3,7 @@ function Promise(executor) {
   //添加属性
   this.PromiseState = 'pending'
   this.PromiseResult = null
+  this.callbacks = []
   // 设置this
   const self = this
 
@@ -34,9 +35,65 @@ function Promise(executor) {
 
 }
 
-// then 方法
-Promise.prototype.then = function () {
+// then 方法, 两个回调，形参两个函数
+Promise.prototype.then = function (onResolved, onRejected) {
+  // 回调函数返回的不是函数时
+  if (typeof onResolved !== 'function') {
+    // 直接返回 值
+    onResolved = value => value
+  }
+  if (typeof onRejected !== 'function') {
+    onRejected = reason => {
+      throw reason
+    }
+  }
+  const self = this
 
+  // 封装函数
+  function callback(type) {
+    try {
+      // 获取回调函数的执行结果
+      // onRejected(this.PromiseResult)
+      let result = type(self.PromiseResult)
+      // 结果值还为 promise
+      if (result instanceof Promise) {
+        result.then(v => {
+          resolve(v)
+        }, r => {
+          reject(r)
+        })
+      } else {
+        // 结果值为 其他
+        resolve(result)
+      }
+    } catch (e) {
+      reject(e)
+    }
+  }
+
+  // 返回的是Promise
+  return new Promise((resolve, reject) => {
+    // 状态为 fulfilled 时
+    if (this.PromiseState === 'fulfilled') {
+      callback(onResolved)
+    }
+    // 状态为 rejected 时
+    if (this.PromiseState === 'rejected') {
+      callback(onRejected)
+    }
+    // 状态为 pending
+    if (this.PromiseState === 'pending') {
+      // 保存回调函数，可能有多个，应使用数组
+      this.callbacks.push({
+        onResolved: function () {
+          callback(onResolved)
+        },
+        onRejected: function () {
+          callback(onRejected)
+        }
+      })
+    }
+  })
 }
 // catch 方法
 Promise.prototype.catch = function () {
